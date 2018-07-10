512781c51e65ab563749bd9ec3608f3fe0e18142
cph <stegben.benjamin@gmail.com>
2018-07-10 18:04:30 +0800

    0.2.0

--- generators/app/index.js
+++ generators/app/index.js
@@ -213,6 +213,7 @@ module.exports = class extends Generator {
     );
 
     cp('.flake8', '.flake8');
+    cp('_Makefile', 'Makefile');
     cp('.editorconfig', '.editorconfig');
     cp('.gitignore', '.gitignore');
     cp('_requirements.txt', 'requirements.txt');
